6e3d6aa4ffe8e3f7cf6ab4e46e6a8ec93f4e598e
Arthur Franco <153031237+Ima23-d@users.noreply.github.com>
2026-08-16 12:36:44 -0300

    Refactor Carro class methods and logic

--- CESMAC 4° periodo/POO/AV1/ATIVIDADES/ATV2/Ex2/Carro.java
+++ CESMAC 4° periodo/POO/AV1/ATIVIDADES/ATV2/Ex2/Carro.java
@@ -1,42 +1,43 @@
-class Carro{
-    
+class Carro {
+
     private String modelo;
     private int velocidade;
     private boolean ligado;
-    
-    public Carro(String modelo, int velocidade, boolean ligado){
+
+    public Carro(String modelo, int velocidade, boolean ligado) {
         this.modelo = modelo;
         this.velocidade = velocidade;
         this.ligado = ligado;
     }
-    
-    public void acelerar(){
-        this.velocidade +=10;
+
+    public void acelerar() {
+        this.velocidade += 10;
     }
-    
-    public void frear(){
-        this.velocidade -=10;
-    }
-    
-    public void ligado(){
-        if(!this.ligado){
-            this.ligado = true;
+
+    public void frear() {
+        if (this.velocidade >= 10) {
+            this.velocidade -= 10;
+        } else {
+            this.velocidade = 0;
         }
     }
-    
-    public void desligado(){
-        if(this.velocidade == 0){
-            if(this.ligado){
-                this.ligado = false;
-            }
-        }
+
+    public void ligar() {
+        this.ligado = true;
     }
-    
+
+    public String desligar() {
+        if (this.velocidade == 0) {
+            this.ligado = false;
+            return "Carro desligado.";
+        }
+        return "Carro ligado.";
+    }
+
     public void exibirInformacoes() {
-        System.out.println("____INFORMAÇÕES____");
+        System.out.println("____INFORMACOES____");
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Velocidade: " + this.velocidade);
         System.out.println("Ligado: " + this.ligado);
     }
-
 }
